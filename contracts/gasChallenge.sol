4f1172bc53eb2c3141cf04da44b56428d9a043db
Chacha-A-Chacha <calex2607@gmail.com>
2023-07-14 02:08:04 +0300

    modified:   contracts/gasChallenge.sol 	modified:   test/test_gasChallenge.js

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    //Fixed-Size Array Technique Here
+    uint256[10] numbers = [1,2,3,4,5,6,7,8,9,10];
     
     //Function to check for sum of array
     //No changes required in this function
@@ -24,6 +24,46 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        uint256 length = numbers.length;
+        assembly {
+            let ptr := numbers.slot
+            for {let i := 0} lt(i, length) {i := add(i, 1)} {
+                sstore(add(ptr, mul(i, 0x20)), 0)
+            }
+        }
     }
  }
+
+
+/*
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract gasChallenge {
+    uint256[10] numbers = [1,2,3,4,5,6,7,8,9,10]; // Fixed-size array
+    
+    function getSumOfArray() public view returns (uint256) {
+        uint256 sum = 0;
+        for (uint256 i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    
+    function notOptimizedFunction() public {
+        for (uint256 i = 0; i < numbers.length; i++) {
+            numbers[i] = 0;
+        }
+    }
+    
+    function optimizedFunction() public {
+        uint256 length = numbers.length;
+        assembly {
+            let ptr := numbers.slot
+            for {let i := 0} lt(i, length) {i := add(i, 1)} {
+                sstore(add(ptr, mul(i, 0x20)), 0)
+            }
+        }
+    }
+}
+*/
