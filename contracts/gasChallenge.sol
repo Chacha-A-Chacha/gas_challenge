0e43236f2095f3b1ead28ed53069a3d4d1737813
Chacha-A-Chacha <calex2607@gmail.com>
2023-07-14 04:33:54 +0300

    new file:   artifacts/build-info/b16b16cf1fcb9ebb084d9ac8d5912659.json 	deleted:    artifacts/build-info/ff57977f8025f44adbb4b0e16e972bd9.json 	modified:   artifacts/contracts/gasChallenge.sol/gasChallenge.dbg.json 	modified:   artifacts/contracts/gasChallenge.sol/gasChallenge.json 	modified:   cache/solidity-files-cache.json 	modified:   contracts/gasChallenge.sol 	modified:   gas-report.txt

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -24,32 +24,12 @@ contract gasChallenge {
     //Implementing The Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should be equal 0
     function optimizedFunction() public {
-        // uint length = numbers.length;
-
-        // for (uint i = 0; i < length; i++) {
-        //     numbers[i] = 0;
-        // }
-
-        // uint length = numbers.length;
-        // uint[10] storage localNumbers = numbers;
-
-        // assembly {
-        //     let ptr := localNumbers.slot
-
-        //     for {
-        //         let i := 0
-        //     } lt(i, length) {
-        //         i := add(i, 1)
-        //     } {
-        //         sstore(add(ptr, mul(i, 0x20)), 0)
-        //     }
-        // }
         uint length = numbers.length; // Cache the length of the `numbers` array
 
         // Cache the `numbers` array to a local storage variable
         uint[10] storage localNumbers = numbers;
 
-        //i++ over i += 1
+        // i++ over i += 1
         // unchecked
         for (uint i = 0; i < length; ) {
             localNumbers[i] = 0;
