96a74eb396f01299ac4ab9a575fc1491f6d1c2f6
Chacha-A-Chacha <calex2607@gmail.com>
2023-07-14 04:31:56 +0300

    deleted:    artifacts/build-info/ff57977f8025f44adbb4b0e16e972bd9.json 	modified:   artifacts/contracts/gasChallenge.sol/gasChallenge.dbg.json 	modified:   artifacts/contracts/gasChallenge.sol/gasChallenge.json 	modified:   cache/solidity-files-cache.json 	modified:   gas-report.txt
     Untracked files:
    	artifacts/build-info/b16b16cf1fcb9ebb084d9ac8d5912659.json

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -49,9 +49,22 @@ contract gasChallenge {
         // Cache the `numbers` array to a local storage variable
         uint[10] storage localNumbers = numbers;
 
-        // i++ over i += 1
-        for (uint i = 0; i < length; i++) {
+        //i++ over i += 1
+        // unchecked
+        for (uint i = 0; i < length; ) {
             localNumbers[i] = 0;
+            unchecked {
+                i++;
+            }
         }
+        //     assembly {
+        //     let ptr := localNumbers.slot
+
+        //     for {let i := 0} lt(i, length) {i := add(i, 1)} {
+        //         unchecked {
+        //             sstore(add(ptr, mul(i, 0x20)), 0)
+        //         }
+        //     }
+        // }
     }
 }
